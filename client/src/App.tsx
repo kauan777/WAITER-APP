5f185a8ebcc55b21dbb8e09dfc15915c3d0e8a33
kauan777 <kauan7622@gmail.com>
2022-12-03 22:51:08 -0300

    structuring page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,13 @@
+import { Header } from "./components/Header";
+import { Orders } from "./components/Orders";
+import { GlobalStyles } from "./styles/GlobalStyles";
+
 export function App() {
-	return <h1>Hello World</h1>;
+	return (
+		<>
+			<GlobalStyles />
+			<Header />
+			<Orders />
+		</>
+	);
 }
